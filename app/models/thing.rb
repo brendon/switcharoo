e293f77741da03921d00301b2629f2328812f276
Brendon Muir <brendon@spike.net.nz>
2021-05-03 11:39:15 +1200

    Create the Thing scaffold

---
+++ app/models/thing.rb
@@ -0,0 +1,3 @@
+class Thing < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_02_233451) do
+
+  create_table "things", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
